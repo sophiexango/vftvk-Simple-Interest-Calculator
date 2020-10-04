0d7fc1d6d5060b1daeb89b910f8f06de1e76e353
sophiexango <71581331+sophiexango@users.noreply.github.com>
2020-10-04 18:47:49 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
+ 
+var  principal,rate,years,interest;
+    
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+    principal = parseFloat(document.getElementById("principal").value);
+    rate = parseFloat(document.getElementById("rate").value);
+    years = parseInt(document.getElementById("years").value);
+    interest = principal * years * rate / 100;
+    var today=new Date();
+    var thisYear=(today.getFullYear());
+    var dueYear=(thisYear + years);
     
+   document.getElementById("result").innerHTML =
+("If you deposit " + "<var>"+ principal +"</var>"+ "<br>" +
+ "at an interest rate of " + "<var>"+ rate+"%</var>.<br>" +
+ "You will receive an amount of "+"<var>"+interest +"</var>"+"<br>" +
+ "in the year " +"<var>"+dueYear+"</var>");
 }
-        
+
+function checkprincipal()
+{
+ var principalcheck = parseFloat(document.getElementById("principal").value);
+ if (principalcheck <= 0)
+ {alert("Enter a positive number!");
+ document.getElementById("principal").focus();
+ }
+}
+
+   
+function slider()
+{
+    var slider = parseFloat(document.getElementById("rate").value);
+    document.getElementById("rateview").innerHTML = (slider+" %");
+}
+
